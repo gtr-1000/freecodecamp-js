0987a75d70846cf6c251020ad20268815eb5430f
gtr-1000 <dalbertvinicius5@gmail.com>
2026-07-17 20:45:52 -0300

    Add email masking function using indexOf

---
+++ basic-algorithm-scripting/mask-email/maskEmail-indexOf.js
@@ -0,0 +1,14 @@
+function maskEmail(email) {
+  const atIndex = email.indexOf("@");
+  const localPart = email.slice(0, atIndex);
+  const domain = email.slice(atIndex); // já inclui o "@"
+
+  const firstChar = localPart.slice(0, 1);
+  const lastChar = localPart.slice(-1);
+  const middleMask = "*".repeat(localPart.length - 2);
+
+  return firstChar + middleMask + lastChar + domain;
+}
+
+const email = "freecodecamp@example.com";
+console.log(maskEmail(email));
